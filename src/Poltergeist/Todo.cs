64bc4e83c96c868ba5b4e5b45df157c2922e054c
Fortelle <fortelle@outlook.com>
2022-10-07 22:57:14 +0800

    Update Todo.cs

--- src/Poltergeist/Todo.cs
+++ src/Poltergeist/Todo.cs
@@ -1,18 +1,16 @@
-﻿internal class Todo
-{
-    // Scheduler
-
-    // Settings
-
-    // ToastService
-
-    // Two mode (task/thread)
-
-    // Multiple macro tabs
-
-    // suspend/resume support
-
-    // main menu/restart/restart(admin)
-
-    // doc comments
-}
+﻿/*
+ * todo:
+ * 
+ * Scheduler
+ * ToastService
+ * Two mode (task/thread)
+ * Multiple macro tabs
+ * suspend/resume support
+ * main menu/restart/restart(admin)
+ * doc comments
+ * HideWindow, Shutdown
+ * restart computer and resume 
+ * hook args
+ * options -> object properties
+ * optioninfo
+ */
