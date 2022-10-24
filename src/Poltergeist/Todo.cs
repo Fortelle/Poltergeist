374f3e65b7db43179048021c2f6a3c0e2ecfe5a9
Fortelle <fortelle@outlook.com>
2022-10-24 21:54:44 +0800

    Update Todo.cs

--- src/Poltergeist/Todo.cs
+++ src/Poltergeist/Todo.cs
@@ -3,16 +3,22 @@
  * 
  * Scheduler
  * ToastService
- * Two mode (task/thread)
  * Multiple macro tabs
  * suspend/resume support
  * main menu/restart/restart(admin)
  * doc comments
- * HideWindow, Shutdown
  * restart computer and resume 
  * hook args
  * options -> object properties
  * optioninfo
  * BackgroundWindows - control-based operator
  * fileformat for custom options
+ * global option accessibility
+ * communication between application and macroprocessor
+ */
+
+/*
+ * issues:
+ * 
+ * fatal: processor.abort conflicts with nested trycatch
  */
